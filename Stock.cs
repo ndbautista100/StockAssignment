689f95b6c4dc95f5589db06e4287007ae6df1ae7
ndbautista100 <47700739+ndbautista100@users.noreply.github.com>
2022-02-03 11:01:51 -0800

    Add project files.

---
+++ Stock.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System;
+
+namespace StockAssignment
+{
+
+    //-----------------------------------------------------------------------------------
+    public class Stock
+    {
+
+        public event EventHandler<StockNotification> StockEvent;
+        public event EventHandler<EventArgs> FileStockEvent;
+        //Name of our stock.
+        private string _name;
+        //Starting value of the stock.
+        private int _initialValue;
+        //Max change of the stock that is possible.
+        private int _maxChange;
+        //Threshold value where we notify subscribers to the event.
+        private int _threshold;
+        //Amount of changes the stock goes through.
+        private int _numChanges;
+        //Current value of the stock.
+        private int _currentValue;
+        private readonly Thread _thread;
+        public string StockName { get; set; }
+        public int InitialValue { get; set;}
+        public int CurrentValue { get; set; }
+        public int MaxChange { get; set; }
+        public int Threshold { get; set; }
+        public int NumChanges { get; set; }
+        //-----------------------------------------------------------------------------
+        /// <summary>
+        /// Stock class that contains all the information and changes of the stock
+        /// </summary>
+        /// <param name="name">Stock name</param>
+        /// <param name="startingValue">Starting stock value</param>
+        /// <param name="maxChange">The max value change of the stock</param>
+        /// <param name="threshold">The range for the stock</param>
+        public Stock(string name, int startingValue, int maxChange, int threshold)
+        {
+            StockName = name;
+            InitialValue = startingValue;
+            CurrentValue = startingValue;
+            MaxChange = maxChange;
+            Threshold = threshold;
+            this._thread = new Thread(new ThreadStart(Activate));
+            _thread.Start();
+        }
+
+        public void Activate()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Thread.Sleep(500); // 1/2 second
+                ChangeStockValue();
+            }
+        }
+
+
+        public void ChangeStockValue()
+        {
+            var rand = new Random();
+            CurrentValue += Convert.ToInt32(rand.NextDouble() * (MaxChange - (MaxChange * -1)) + (MaxChange * -1));
+            NumChanges++;
+            var newStock = new StockNotification(_name, _currentValue, _numChanges);
+            if (Math.Abs((CurrentValue - InitialValue)) > Threshold)
+            { //RAISE THE EVENT
+                StockEvent?.Invoke(this, newStock );
+                FileStockEvent?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+
+    }
+}
---
+++ stockbroker.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace StockAssignment
+{
+    class StockBroker
+    {
+        public string BrokerName { get; set; }
+        public List<Stock> stocks = new List<Stock>();
+        public static ReaderWriterLockSlim myLock = new ReaderWriterLockSlim();
+        //readonly string docPath = @"C:\Users\Documents\CECS 475\Lab3_output.txt";
+        readonly string destPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+       "Lab1_output.txt");
+        public string path = @"D:\c#\MyTest.txt";
+        public string titles = "Broker".PadRight(10) + "Stock".PadRight(15) +
+        "Value".PadRight(10) + "Changes".PadRight(10) + "Date and Time";
+
+        public StockBroker(string brokerName)
+        {
+            BrokerName = brokerName;
+            Console.WriteLine(titles);
+        }
+
+        public void AddStock(Stock stock)
+        {
+            stocks.Add(stock);
+            stock.StockEvent += EventHandler;
+            stock.FileStockEvent += TextFileEventHandler;
+        }
+
+       
+
+        void EventHandler(Object sender, StockNotification e)
+        {
+            try
+            { //LOCK Mechanism
+                
+                Stock newStock = (Stock)sender;
+                string statement = BrokerName.PadRight(15) + newStock.StockName.PadRight(15) + newStock.CurrentValue.ToString().PadRight(10) + newStock.NumChanges.ToString().PadRight(10) + DateTime.Now.ToString();
+
+                Console.WriteLine(statement);
+                //string statement;
+                //!NOTE!: Check out C#events, pg.4
+                // Display the output to the console windows
+                Console.WriteLine(BrokerName.PadRight(16));
+               /*______________________________________________);
+                //Display the output to the file
+                using (StreamWriter outputFile
+               = ________________________________________________)
+                {
+                    ________________________________________________
+                }*/
+                //RELEASE the lock
+                
+            }
+            finally
+            {
+            }
+        }
+
+        void TextFileEventHandler(Object sender, EventArgs e)
+        {
+            try
+            {
+                
+                Stock newStock = (Stock)sender;
+                string statement = BrokerName.PadRight(15) + newStock.StockName.PadRight(15) + newStock.CurrentValue.ToString().PadRight(10) + newStock.NumChanges.ToString().PadRight(10) + DateTime.Now.ToString();
+                myLock.EnterWriteLock();
+                if (!File.Exists(path))
+                {
+                   
+
+                    using (TextWriter tw = new StreamWriter(path))
+                    {
+                        tw.WriteLine(statement);
+                    }
+
+                }
+                else if (File.Exists(path))
+                {
+                    using (TextWriter tw = new StreamWriter(path))
+                    {
+                        tw.WriteLine(statement);
+                    }
+                }
+            }
+            finally
+            {
+                myLock.ExitWriteLock();
+            }
+        }
+    }
+    
+}
